d70ea015d9678137ad7bf8eff6c7da42a2c8fe70
AlejandraKeber <alejandrakeber@gmail.com>
2023-01-10 14:43:55 -0500

    Update App and index.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React from 'react';
 import Calculator from './components/Calculator';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = '';
+  }
+
   render() {
     return (
       <div className="App">
