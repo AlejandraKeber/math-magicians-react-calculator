42621a76e121f00e30e6f1447fb769a98c2c1520
AlejandraKeber <alejandrakeber@gmail.com>
2023-01-10 14:10:43 -0500

    Create components folder

---
+++ src/components/Calculator.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Calculator.css';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = '';
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Probando</p>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
